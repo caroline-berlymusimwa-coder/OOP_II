5964afa7a03fc6e8fc5d138d16acabaaf3716922
Musimwa <caroline-berly.musimwa@strathmore.edu>
2026-04-17 14:36:53 +0300

    ïnitial commit

---
+++ src/com/test_vehicle/Automobile.java
@@ -0,0 +1,10 @@
+package com.test_vehicle;
+
+public interface Automobile
+{
+    public abstract void accelerate();
+    public abstract void brake();
+    public abstract void stop();
+    public abstract void gas();
+
+}
---
+++ src/com/test_vehicle/vehicle.java
@@ -0,0 +1,15 @@
+package com.test_vehicle;
+
+public abstract class vehicle {
+    public abstract void accelerate();
+    public abstract void  brake();
+    public abstract void  stop();
+    public abstract void  gas();
+    public abstract void  fuel();
+    public abstract void  load();
+    public abstract void  sound();
+
+}
+//public public static void main(String[] args) {
+
+//}
